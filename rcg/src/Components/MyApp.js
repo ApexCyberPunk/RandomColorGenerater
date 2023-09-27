e7de21f2b63d587718519c736069cbd47bd58c35
snovacain <61154743+snovacain@users.noreply.github.com>
2023-09-26 23:48:53 -0700

    update

---
+++ rcg/src/Components/MyApp.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react'
+import Color from './color'
+
+export default class MyApp extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            colorNum : 1,
+            colors: []
+        }
+        for(let i=0; i < this.state.colorNum; i++) {
+            this.state.colors.push({hexCode : this.generateColor()})
+        }
+    }
+
+    // generateColor 
+
+    generateColor() {
+        return '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+    }
+
+    updateColor(index) {
+        let colors = this.state.colors.slice();
+        const currentColor = this.generateColor();
+        colors[index].hexCode = currentColor;
+        this.setState(
+            {
+                colors : colors
+            }
+        )
+    }
+
+  render() {
+    return (
+      <div className="color-container">
+       {this.state.colors.map((color,index) =>
+        <Color key={`color-${index}`} index={index} 
+            update={this.updateColor.bind(this)}
+            hexCode = {color.hexCode}
+        />
+       )}
+      </div>
+    )
+  }
+}
---
+++ rcg/src/Components/color.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+
+
+class color extends React.Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default color
